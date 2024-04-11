afbb889b5d890db719d492ce0d68ea29a776226c
Haoxiang Lin <124523713+Life1li@users.noreply.github.com>
2024-04-11 14:15:08 -0500

    Updates

--- generate.py
+++ generate.py
@@ -1,9 +1,10 @@
 import argparse
 import json
 import re
-
 import numpy as np
+from googletrans import Translator
 
+# Setup argument parser
 parser = argparse.ArgumentParser(description='Get text from model')
 parser.add_argument('-m', '--model', type=str, help='path to the file from which the model is loaded.',
                     default="all_grams.txt")
@@ -14,6 +15,14 @@ parser.add_argument('-pre', '--prefix', nargs='+',
 parser.add_argument('-len', '--length', type=int, help='length of the generated sequence.', default=20)
 args = parser.parse_args()
 
+def translate_text(text, dest_language="en"):
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=dest_language)
+        return translated.text
+    except Exception as e:
+        print(f"Translation failed: {e}")
+        return text
 
 def clear_prefix(text):
     text = re.sub(r'[^\w\s]+|[\d]+', r'', text).strip()
@@ -21,7 +30,6 @@ def clear_prefix(text):
     text = text.lower()
     text = text.replace('\n', ' ')
     return text
-
 
 def separate_grams(all_grams):
     one_grams = {}
@@ -34,22 +42,17 @@ def separate_grams(all_grams):
             two_grams[gram] = all_grams[gram]
         elif len(gram.split(' ')) == 3:
             three_grams[gram] = all_grams[gram]
-
     return one_grams, two_grams, three_grams
-
 
 def make_text(all_grams, length, prefix=None):
     one_grams, two_grams, three_grams = separate_grams(all_grams)
-    words = one_grams.keys()
-    words = list(words)
+    words = list(one_grams.keys())
 
     arr = []
     if prefix is None or prefix == "":
         arr = np.random.choice(list(two_grams.keys()), 1)[0].split(' ')
-    # if the prefix is of length 1, we will augment to 2 grams for further correct operation
     else:
-        # edits with a prefix
-        prefix = clear_prefix(prefix)
+        prefix = clear_prefix(' '.join(prefix))  # Corrected prefix handling
         arr = prefix.split(" ")
         if len(arr) == 1:
             p_arr = []
@@ -58,32 +61,34 @@ def make_text(all_grams, length, prefix=None):
                     p_arr.append(two_grams[arr[-1] + " " + word] / one_grams[arr[-1]])
                 else:
                     p_arr.append(0)
-            # If there is no 2-gram that has a prefix at the beginning, we add a random 2-gram.
             if sum(p_arr) == 0:
                 arr += np.random.choice(list(two_grams.keys()), 1)[0].split(' ')
             else:
-                # normalize
                 p_arr /= np.sum(p_arr)
                 arr.append(np.random.choice(words, 1, p=p_arr)[0])
 
-    arr = list(arr)
-    for j in range(length):
+    for j in range(length - len(arr)):
         p_arr = []
         for word in words:
             if arr[-2] + " " + arr[-1] + " " + word in three_grams.keys():
                 p_arr.append(three_grams[arr[-2] + " " + arr[-1] + " " + word] / two_grams[arr[-2] + " " + arr[-1]])
             else:
                 p_arr.append(0)
-
-        # there may be no continuation found, then we add a random 2 grams
         if np.sum(p_arr) == 0:
             arr += np.random.choice(list(two_grams.keys()), 1)[0].split(' ')
         else:
-            # normalize
             p_arr /= np.sum(p_arr)
             arr.append(np.random.choice(words, 1, p=p_arr)[0])
-    print(" ".join(arr))
 
+    # Generate the text
+    generated_text = " ".join(arr)
+
+    # Translate the generated text
+    translated_text = translate_text(generated_text, "en")
+
+    # Print both original and translated text
+    print("Original:", generated_text)
+    print("Translated:", translated_text)
 
 if __name__ == '__main__':
     model = args.model
@@ -91,5 +96,5 @@ if __name__ == '__main__':
         all_grams = json.load(file)
 
     length = args.length
-    prefix = args.prefix
+    prefix = args.prefix  # Prefix is now correctly handled within make_text
     make_text(all_grams, length, prefix)
