ca0d43c6eac780464ea6f80efb220c5923051073
Haoxiang Lin <124523713+Life1li@users.noreply.github.com>
2024-04-28 00:18:51 -0500

    Update generate_lyrics.py

--- generate_lyrics.py
+++ generate_lyrics.py
@@ -1,48 +1,68 @@
+import argparse
 import json
-import re
-import time
-import lyricsgenius as genius
+import numpy as np
+import pronouncing  # for rhyme and syllable counting
 
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Generate rhyming song verses')
+parser.add_argument('-m', '--model', type=str, help='Path to the model file', default="all_grams.txt")
+parser.add_argument('-len', '--length', type=int, help='Number of stanzas to generate', default=1)
+args = parser.parse_args()
 
-def make_data():
-    with open('artist.json', encoding='utf-8') as f:
-        aux = json.load(f)
-    lyrics = [song['lyrics'] for song in aux['songs']]
-    titles = [song['title'] for song in aux['songs']]
-    for i in range(len(titles)):
-        lyric = lyrics[i]
-        title = titles[i]
+def load_model(model_path):
+    with open(model_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
-        file = aux['name'] + " " + title
-        file = re.sub(r'[^\w\s]+|[\d]+', r'', file).strip()
-        file = "data/" + file + ".txt"
-        with open(file, mode='w+', encoding='utf-8') as f:
-            f.write(lyric)
+def find_rhymes(word):
+    rhymes = pronouncing.rhymes(word)
+    return rhymes if rhymes else [word]
 
+def generate_line(all_grams, rhyme_word=None, words_limit=7):
+    line = []
+    words_count = 0
+    
+    if rhyme_word:
+        # Choose a rhyming word from the available rhymes that is also in the model
+        rhyme_candidates = [word for word in find_rhymes(rhyme_word) if word in all_grams and all_grams[word] > 0]
+        if rhyme_candidates:
+            end_word = np.random.choice(rhyme_candidates)
+        else:
+            end_word = rhyme_word  # fallback if no suitable rhymes found
+        
+        line.append(end_word)
+        all_grams[end_word] -= 1
+        words_count += 1
 
-def download_dataset(artist):  # api call
-    api = genius.Genius('bfCIzRNIVD5S6qjz2A59gMjMso6SNsFHkPcOY_yqNbmPubaFWokKXKwc6Vo2mKau')
-    artist = api.search_artist(artist, max_songs=200, get_full_info=False)
-    artist.save_lyrics(filename='artist', overwrite=True, verbose=True)
+    # Fill the rest of the line up to the word limit
+    while words_count < words_limit:
+        possible_words = [word for word in all_grams.keys() if all_grams[word] > 0]
+        if not possible_words:
+            break
+        word = np.random.choice(possible_words)
+        line.insert(0, word)  # add to the beginning to maintain rhyme at end
+        all_grams[word] -= 1
+        words_count += 1
 
+    return ' '.join(line)
 
-def choose_artist():
-    artists = ["KIDZ BOP"]
+def generate_stanza(all_grams, words_limit=4):
+    # Generate the first two lines with an AABB rhyme scheme
+    first_line = generate_line(all_grams, words_limit=words_limit)
+    second_line = generate_line(all_grams, rhyme_word=first_line.split()[-1], words_limit=words_limit)
+    third_line = generate_line(all_grams, words_limit=words_limit)
+    fourth_line = generate_line(all_grams, rhyme_word=third_line.split()[-1], words_limit=words_limit)
 
-    for artist in artists:
-        download_dataset(artist)
-        make_data()
-        time.sleep(5)
+    return first_line + '\n' + second_line + '\n' + third_line + '\n' + fourth_line
 
-# script was used to download lyrics of arbitrary artists specified in the artists array from genius.com
 if __name__ == '__main__':
-    choose_artist()
+    model_path = args.model
+    num_stanzas = args.length
+    all_grams = load_model(model_path)
 
+    stanzas = []
+    for _ in range(num_stanzas):
+        stanza = generate_stanza(all_grams)
+        stanzas.append(stanza)
 
-# script was used to download lyrics of arbitrary artists specified in the artists array from genius.com
-if __name__ == '__main__':
-    choose_artist()
-
-    length = args.length
-    prefix = args.prefix  # Prefix is now correctly handled within make_text
-    make_text(all_grams, length, prefix)
+    # Print all stanzas separated by two new lines for clarity
+    print("\n\n".join(stanzas))
