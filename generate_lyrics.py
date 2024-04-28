a84450718b6712f2133f3e2448304fd00a6c8bab
Haoxiang Lin <124523713+Life1li@users.noreply.github.com>
2024-04-27 22:13:51 -0500

    Update generate_lyrics.py

--- generate_lyrics.py
+++ generate_lyrics.py
@@ -1,99 +1,47 @@
-import argparse
 import json
 import re
-import numpy as np
-from googletrans import Translator
+import time
+import lyricsgenius as genius
 
-# Setup argument parser
-parser = argparse.ArgumentParser(description='Get text from model')
-parser.add_argument('-m', '--model', type=str, help='path to the file from which the model is loaded.',
-                    default="all_grams.txt")
-parser.add_argument('-pre', '--prefix', nargs='+',
-                    help='optional argument. The beginning of the sentence (one or more words). If not specified, '
-                         'select the start word randomly from all words.',
-                    default=None)
-parser.add_argument('-len', '--length', type=int, help='length of the generated sequence.', default=20)
-args = parser.parse_args()
 
-def translate_text(text, dest_language="en"):
-    translator = Translator()
-    try:
-        translated = translator.translate(text, dest=dest_language)
-        return translated.text
-    except Exception as e:
-        print(f"Translation failed: {e}")
-        return text
+def make_data():
+    with open('artist.json', encoding='utf-8') as f:
+        aux = json.load(f)
+    lyrics = [song['lyrics'] for song in aux['songs']]
+    titles = [song['title'] for song in aux['songs']]
+    for i in range(len(titles)):
+        lyric = lyrics[i]
+        title = titles[i]
 
-def clear_prefix(text):
-    text = re.sub(r'[^\w\s]+|[\d]+', r'', text).strip()
-    text = text.strip()
-    text = text.lower()
-    text = text.replace('\n', ' ')
-    return text
+        file = aux['name'] + " " + title
+        file = re.sub(r'[^\w\s]+|[\d]+', r'', file).strip()
+        file = "data/" + file + ".txt"
+        with open(file, mode='w+', encoding='utf-8') as f:
+            f.write(lyric)
 
-def separate_grams(all_grams):
-    one_grams = {}
-    two_grams = {}
-    three_grams = {}
-    for gram in all_grams.keys():
-        if len(gram.split(' ')) == 1:
-            one_grams[gram] = all_grams[gram]
-        elif len(gram.split(' ')) == 2:
-            two_grams[gram] = all_grams[gram]
-        elif len(gram.split(' ')) == 3:
-            three_grams[gram] = all_grams[gram]
-    return one_grams, two_grams, three_grams
 
-def make_text(all_grams, length, prefix=None):
-    one_grams, two_grams, three_grams = separate_grams(all_grams)
-    words = list(one_grams.keys())
+def download_dataset(artist):  # api call
+    api = genius.Genius('bfCIzRNIVD5S6qjz2A59gMjMso6SNsFHkPcOY_yqNbmPubaFWokKXKwc6Vo2mKau')
+    artist = api.search_artist(artist, max_songs=200, get_full_info=False)
+    artist.save_lyrics(filename='artist', overwrite=True, verbose=True)
 
-    arr = []
-    if prefix is None or prefix == "":
-        arr = np.random.choice(list(two_grams.keys()), 1)[0].split(' ')
-    else:
-        prefix = clear_prefix(' '.join(prefix))  # Corrected prefix handling
-        arr = prefix.split(" ")
-        if len(arr) == 1:
-            p_arr = []
-            for word in words:
-                if arr[-1] + " " + word in two_grams.keys():
-                    p_arr.append(two_grams[arr[-1] + " " + word] / one_grams[arr[-1]])
-                else:
-                    p_arr.append(0)
-            if sum(p_arr) == 0:
-                arr += np.random.choice(list(two_grams.keys()), 1)[0].split(' ')
-            else:
-                p_arr /= np.sum(p_arr)
-                arr.append(np.random.choice(words, 1, p=p_arr)[0])
 
-    for j in range(length - len(arr)):
-        p_arr = []
-        for word in words:
-            if arr[-2] + " " + arr[-1] + " " + word in three_grams.keys():
-                p_arr.append(three_grams[arr[-2] + " " + arr[-1] + " " + word] / two_grams[arr[-2] + " " + arr[-1]])
-            else:
-                p_arr.append(0)
-        if np.sum(p_arr) == 0:
-            arr += np.random.choice(list(two_grams.keys()), 1)[0].split(' ')
-        else:
-            p_arr /= np.sum(p_arr)
-            arr.append(np.random.choice(words, 1, p=p_arr)[0])
+def choose_artist():
+    artists = ["KIDZ BOP"]
 
-    # Generate the text
-    generated_text = " ".join(arr)
+    for artist in artists:
+        download_dataset(artist)
+        make_data()
+        time.sleep(5)
 
-    # Translate the generated text
-    translated_text = translate_text(generated_text, "en")
-
-    # Print both original and translated text
-    print("Original:", generated_text)
-    # print("Translated:", translated_text)
-
+# script was used to download lyrics of arbitrary artists specified in the artists array from genius.com
 if __name__ == '__main__':
-    model = args.model
-    with open(model, 'r', encoding='utf-8') as file:
-        all_grams = json.load(file)
+    choose_artist()
+
+
+# script was used to download lyrics of arbitrary artists specified in the artists array from genius.com
+if __name__ == '__main__':
+    choose_artist()
 
     length = args.length
     prefix = args.prefix  # Prefix is now correctly handled within make_text
